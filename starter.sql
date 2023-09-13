c57b416d23a85b555af974077af1a7e900aaad27
yosytuvy <yoseftoovian@gmail.com>
2023-09-13 11:37:12 +0300

    solved second Q

--- starter.sql
+++ starter.sql
@@ -22,3 +22,19 @@ VALUES ('John', 'Doe', '1990-01-15', 'john.doe@example.com')
        ('Emily', 'Davis','1993-03-25', 'emily.davis@example.com');
 
 
+-- 2
+
+CREATE TABLE teachers (
+    teacher_id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    date_of_birth DATE,
+    email TEXT UNIQUE,
+    department TEXT,
+    hire_date DATE
+);
+
+INSERT INTO teachers(first_name, last_name, date_of_birth, email, department, hire_date)
+VALUES ('Professor', 'Anderson', '1975-04-08', 'prof.anderson@example.com', 'Computer Science', '2010-08-15'),
+       ('Dr. Sarah', 'Wilson', '1980-12-20' ,'sarah.wilson@example.com', 'Mathematics', '2015-05-02'),
+       ('Mr. James', 'Brown','1978-06-14', 'james.brown@example.com', 'History', '2009-11-11');
