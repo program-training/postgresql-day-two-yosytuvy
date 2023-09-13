84a44cbfda22ec4f03653b93e9e271336fb239b5
yosytuvy <yoseftoovian@gmail.com>
2023-09-13 11:59:39 +0300

    solved part 2 Q 2

--- part 2.sql
+++ part 2.sql
@@ -15,3 +15,18 @@ VALUES (1, 1),
        (3, 2),
        (4, 3),
        (1, 3);
+
+
+-- 2
+CREATE TABLE teacherCourse  (
+    teacherCourse_id SERIAL PRIMARY KEY,
+    teacher_id  INTEGER,
+    course_id  INTEGER,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+INSERT INTO teacherCourse(teacher_id, course_id)
+VALUES (1, 1),
+       (2, 2),
+       (3, 3);
